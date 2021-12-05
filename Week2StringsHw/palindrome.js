0f5c86491392bde098b64003fa264f0fa2730fd7
ImAMyth <mmaloney31@yahoo.com>
2021-12-04 20:19:08 -0500

    Palindrome program complete

--- Week2StringsHw/palindrome.js
+++ Week2StringsHw/palindrome.js
@@ -1,1 +1,11 @@
 // Write a program to find if a user entered string is a palindrome or not (for example "racecar" is a palindrome)
+
+const prompt = require("prompt-sync") ()
+let userStr = prompt("Enter a word :")
+let reverseStr = userStr.split('').reverse().join("")
+if (userStr.toLowerCase() ===  reverseStr.toLowerCase()) {
+    console.log("Palindrome");
+}
+else{
+    console.log("Not a Palindrome");
+}
