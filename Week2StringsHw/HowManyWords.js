9261ad290d37c4720f9f4cece3ae520c4e279a98
ImAMyth <mmaloney31@yahoo.com>
2021-12-05 00:04:48 -0500

    Completed program to find how many times a user entered word occurs

--- Week2StringsHw/HowManyWords.js
+++ Week2StringsHw/HowManyWords.js
@@ -1,1 +1,13 @@
 // Write a program to find how many times a user entered word occurs in given string
+
+let prompt = require("prompt-sync")()
+let str = "We went to the store and bought two and a half pounds of chicken"
+let strArr = str.toLowerCase().split(" ")
+let wordCheck = prompt("Enter a word to be checked : ")
+let count = 0
+for (let i = 0; i < strArr.length; i++) {
+        if(wordCheck === strArr[i])
+        count++
+       
+}
+console.log(count)
