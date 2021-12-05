41ffcbcdd49de647b43b44e8b94ba7f59a02433c
ImAMyth <mmaloney31@yahoo.com>
2021-12-04 21:22:49 -0500

    Completed program to find how many numbers , letters and special characters in a string

--- Week2StringsHw/HowMany.js
+++ Week2StringsHw/HowMany.js
@@ -1,1 +1,26 @@
 // Write a program to find how many letters, numbers, and special characters are in a user entered string.
+
+let prompt = require("prompt-sync")()
+let userStr = prompt("Enter a word or sentence: ")
+let letters = 0
+let numbers = 0
+let specialChars = 0
+
+for (let i = 0; i < userStr.length; i++) {
+
+    if (userStr.toUpperCase().charCodeAt(i) >= 65 && userStr.toUpperCase().charCodeAt(i) <= 90) {
+        letters++
+    }
+    else if (userStr.charCodeAt(i) >= 48 && userStr.charCodeAt(i) <= 57) {
+        numbers++
+    }
+    else {
+        specialChars++
+    }
+
+}
+console.log("" + letters);
+console.log(numbers);
+console.log(specialChars);
+
+  
