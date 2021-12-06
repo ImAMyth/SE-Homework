60bbaa2ce01ba1f43227b2bcf09e36d6fa4ba58f
ImAMyth <mmaloney31@yahoo.com>
2021-12-05 22:04:42 -0500

    Completed capitalize first letter of each word program

--- Week2StringsHw/CapitalizeFirst.js
+++ Week2StringsHw/CapitalizeFirst.js
@@ -1,1 +1,10 @@
 // BONUS: Capitalize the first letter of each word in a given string (or user entered string/sentence)
+
+let prompt = require("prompt-sync")()
+let str = prompt("Enter a word or sentence : ")
+let modStr = str.toLowerCase().split(" ")
+
+for (let i = 0; i < modStr.length; i++) {
+    modStr[i] = modStr[i].charAt(0).toUpperCase() + modStr[i].slice(1)
+}
+console.log(modStr.join(" "))
