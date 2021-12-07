18a0e0e8b54a7fe2dbe2f9f96f6c928fb3f1880f
ImAMyth <mmaloney31@yahoo.com>
2021-12-07 15:47:13 -0500

    Completed program to find third side of a triangle

--- Exercises/PythagoreanTriangle.js
+++ Exercises/PythagoreanTriangle.js
@@ -1,1 +1,8 @@
 //Write a function expression to calculate the third side of a triangle when the user gives the first two sides
+
+const pyTri = function(a, b) {
+     c = (a * a) + (b * b) 
+     console.log( c * c)
+     return c * c
+}
+pyTri(2 , 5)
