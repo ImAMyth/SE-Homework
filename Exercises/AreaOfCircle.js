a9075854abb3795510a9b9721814c3f17b210ceb
ImAMyth <mmaloney31@yahoo.com>
2021-12-07 15:48:09 -0500

    Completed program to calculate area of a circle using a function declaration

--- Exercises/AreaOfCircle.js
+++ Exercises/AreaOfCircle.js
@@ -1,1 +1,7 @@
 //Write a function declaration to calculate the area of a circle.
+let radius = 10
+function areaOfCircle (radius){
+     console.log(Math.PI * radius * radius)
+     return Math.PI * (radius * radius)
+}
+areaOfCircle(radius)
