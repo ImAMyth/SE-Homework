94276356f81aa6daca627ff78db5a6dc7706e38d
ImAMyth <mmaloney31@yahoo.com>
2021-12-12 18:21:03 -0500

    Completed a program that takes three numbers as arguements and returns the largest

--- Week3FunctionsHw/script.js
+++ Week3FunctionsHw/script.js
@@ -1,1 +1,9 @@
 // Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+
+function maxOfThree(num1 , num2 , num3){
+    let arr = []
+    arr.push(num1 , num2 , num3)
+    let arr2 = arr.reduce((prevNum , currNum) => prevNum > currNum ? prevNum : currNum)
+    console.log(arr2);
+}
+maxOfThree(10 , 5 , 15)
