4ee34e8311b2237bce9e5b5ef2097bfbc003cb14
ImAMyth <mmaloney31@yahoo.com>
2021-12-20 13:54:06 -0500

    Added second button to display resolution

--- complaints_app/app.js
+++ complaints_app/app.js
@@ -2,28 +2,39 @@ const buttons = document.querySelectorAll("button")
 const complaints = document.querySelector("#complaints")
 let limit = 10
 
-buttons.forEach( currentButton => {
-currentButton.addEventListener("click", () => {
-    let borough = currentButton.value
-    let userValueLimit = document.querySelector("#complaints-search").value
-  
-    if (userValueLimit){
-     limit = userValueLimit
+buttons.forEach(currentButton => {
+    currentButton.addEventListener("click", () => {
+        let borough = currentButton.value
+        let userValueLimit = document.querySelector("#complaints-search").value
+
+        if (userValueLimit) {
+            limit = userValueLimit
+        }
+
+        let url = `https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=${limit}&$offset=5&borough=${borough}&agency=NYPD`
+        fetch(url)
+            .then((resolution) => resolution.json())
+            .then((data) => {
+                complaints.innerHTML = `<ul>` + data.map((complaint, index) => {
+                    return `<li>${complaint.complaint_type}</li> <button  onclick = "buttonDescription ('description${index}')" type = "button" class = "police"> WHAT DID THE POLICE DO?</button> <div class = "descriptions" id = "description${index}"> ${complaint.resolution_description}</div>`
+                }).join(``) + `</ul>`
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    })
+})
+
+function buttonDescription(tagId) {
+    let id = document.querySelector("#" + tagId)
+    id.style.display = "none"
+    if (id.style.display == "none") {
+        id.style.display = 'block'
+
     }
-
-    let url = `https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=${limit}&$offset=5&borough=${borough}&agency=NYPD`
-    fetch(url)
-    .then((resolution) => resolution.json())
-    .then((data) => {
-        complaints.innerHTML = `<ul>` + data.map(complaint => {
-        return `<li>${complaint.complaint_type}</li> <button type = "button" class = "police"> WHAT DID THE POLICE DO?</button>`
-        }).join(``) + `</ul>`
-   
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-})
-})
-
+    else {
+        id.style.display = 'none'
+    }
+}
 
